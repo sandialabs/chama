0b3d7013aedc967965d86311b3b3e10f774545a3
Carl Laird <carldlaird@users.noreply.github.com>
2017-09-05 08:51:52 -0500

    utils functions now take scalar or list of types - fixed bugs in utils.py - added tests for utils.py

---
+++ chama/tests/test_utils.py
@@ -0,0 +1,125 @@
+import unittest
+import pandas as pd
+import numpy as np
+import chama
+import chama.utils as cu
+
+#sample_data = [{'Col1': 's1', 'Col2': 1.11, 'Col3', 1},
+#               {'Col1': 's2', 'Col2': 2.22, 'Col3', 2},
+#               {'Col1': 's3', 'Col2': 3.33, 'Col3', 3},
+#               {'Col1': 's4', 'Col2': 4.44, 'Col3', 4}
+#               ]
+
+
+sample_data = {'Col1': ['s1', 's2', 's3'],
+                'Col2': [1.11, 2.22, 3.33],
+                'Col3': [1, 2, 3]}
+
+sample_data2 = {'Col1': ['s1', 's2', 's3'],
+                'Col2': [1.11, np.nan, 3.33],
+                'Col3': [1, 2, 3]}
+
+class UtilsTests(unittest.TestCase):
+    
+    def test_df_columns_required(self):
+        df = pd.DataFrame.from_dict(sample_data)
+
+        # test all good - raises exception (and fails test if not)
+        cu.df_columns_required('sample_data', df, {'Col1': object, 'Col2': np.float64, 'Col3': np.int64})
+
+        # test incorrect type - object
+        with self.assertRaises(TypeError):
+            cu.df_columns_required('sample_data', df, {'Col1': str, 'Col2': np.float64, 'Col3': np.int64})
+
+        # test incorrect type - float
+        with self.assertRaises(TypeError):
+            cu.df_columns_required('sample_data', df, {'Col1': object, 'Col2': str, 'Col3': np.int64})
+
+        # test incorrect type - int
+        with self.assertRaises(TypeError):
+            cu.df_columns_required('sample_data', df, {'Col1': object, 'Col2': np.float64, 'Col3': np.float64})
+
+        # test passing multiple types
+        cu.df_columns_required('sample_data', df, {'Col1': object, 'Col2': [np.int64, np.float64], 'Col3': [np.int64, np.float64]})
+
+        # test passing multiple types with incorrect types
+        with self.assertRaises(TypeError):
+            cu.df_columns_required('sample_data', df, {'Col1': object, 'Col2': [np.int64, np.bool8], 'Col3': [np.int64, np.float64]})
+
+    def test_df_columns_exist(self):
+        df = pd.DataFrame.from_dict(sample_data)
+
+        # test all good - raises exception (and fails test if not)
+        self.assertTrue(cu.df_columns_exist(df, {'Col1': object, 'Col2': np.float64, 'Col3': np.int64})) 
+
+        # test incorrect type - object
+        self.assertFalse(cu.df_columns_exist(df, {'Col1': str, 'Col2': np.float64, 'Col3': np.int64}))
+
+        # test incorrect type - float
+        self.assertFalse(cu.df_columns_exist(df, {'Col1': object, 'Col2': str, 'Col3': np.int64}))
+        
+        # test incorrect type - int
+        self.assertFalse(cu.df_columns_exist(df, {'Col1': object, 'Col2': np.float64, 'Col3': np.float64}))
+
+        # test passing multiple types
+        self.assertTrue(cu.df_columns_exist(df, {'Col1': object, 'Col2': [np.int64, np.float64], 'Col3': [np.int64, np.float64]}))
+
+        # test passing multiple types with incorrect types
+        self.assertFalse(cu.df_columns_exist(df, {'Col1': object, 'Col2': [np.int64, np.bool8], 'Col3': [np.int64, np.float64]}))
+
+    def test_df_columns_has_nans(self):
+        df = pd.DataFrame.from_dict(sample_data2)
+
+        # test for NAN in col2 (True)
+        self.assertTrue(cu.df_columns_has_nans(df, 'Col2'))
+
+        # test for NAN in col3 (False)
+        self.assertFalse(cu.df_columns_has_nans(df, 'Col3'))
+
+        # test for NAN in col2 or col3 (True)
+        self.assertTrue(cu.df_columns_has_nans(df, ['Col2', 'Col3']))
+
+        # test for NAN in col1 or col3 (False)
+        self.assertFalse(cu.df_columns_has_nans(df, ['Col1', 'Col3']))
+        
+        # test for exception if column name does not exist
+        with self.assertRaises(TypeError):
+            cu.df_columns_has_nans(df, ['Col1', 'ColDoesNotExist'])
+        with self.assertRaises(TypeError):
+            cu.df_columns_has_nans(df, 'ColDoesNotExist')
+
+
+    def test_df_columns_nans_not_allowed(self):
+        df = pd.DataFrame.from_dict(sample_data2)
+
+        # test for NAN in col2 (True)
+        with self.assertRaises(TypeError):
+            cu.df_columns_nans_not_allowed('sample_data2', df, 'Col2')
+
+        # test for NAN in col3 (False)
+        cu.df_columns_nans_not_allowed('sample_data2', df, 'Col3')
+
+        # test for NAN in col2 or col3 (True)
+        with self.assertRaises(TypeError):
+            cu.df_columns_nans_not_allowed('sample_data2', df, ['Col2', 'Col3'])
+
+        # test for NAN in col1 or col3 (False)
+        cu.df_columns_nans_not_allowed('sample_data2', df, ['Col1', 'Col3'])
+        
+        # test for exception if column name does not exist
+        with self.assertRaises(TypeError):
+            cu.df_columns_nans_not_allowed('sample_data2', df, ['Col1', 'ColDoesNotExist'])
+        with self.assertRaises(TypeError):
+            cu.df_columns_nans_not_allowed('sample_data2', df, 'ColDoesNotExist')
+
+    def test_df_nans_not_allowed(self):
+        df = pd.DataFrame.from_dict(sample_data)
+        df2 = pd.DataFrame.from_dict(sample_data2)
+
+        cu.df_nans_not_allowed('sample_data', df)
+
+        with self.assertRaises(TypeError):
+           cu.df_nans_not_allowed('sample_data2', df2) 
+        
+        
+
--- chama/utils.py
+++ chama/utils.py
@@ -1,30 +1,55 @@
 """
 The utils module contains a collection of utility functions.
 """
+def _scalar_or_list_to_list(data):
+        if isinstance(data, list):
+            return [ i for i in data ]
+        else:
+            return [ data ]
 
 def df_columns_required(df_name, df, col_type_dict):
+    """ Internal function that raises an exception if dataframe does not contain the expected columns with expected types
+    """
     for k,v in col_type_dict.items():
         if not df_columns_exist(df, {k:v}):
             raise TypeError('Expected column "{0}" of type {1} in DataFrame "{2}."'.format(k, v, df_name))
 
 def df_columns_exist(df, col_type_dict):
-    for name, dtype in col_type_dict.items():
-        if name not in df or df[name].dtype != dtype:
+    """ Internal function that returns False if a column in the dataframe does not exist or is not of the expected type
+    """
+    for name, dtypes in col_type_dict.items():
+        if name not in df:
             return False
+
+        new_dtypes = _scalar_or_list_to_list(dtypes)
+        if df[name].dtype not in new_dtypes:
+            return False
+
     return True
 
 def df_nans_not_allowed(df_name, df):
+    """ Internal function that raises an exception if a user passed a dataframe with NANs when not allowed.
+    """
     if df.isnull().values.any():
         raise TypeError('Found unexpected NaN values in DataFrame "{0}".'.format(df_name))
 
 def df_columns_nans_not_allowed(df_name, df, col_list):
-    for name in col_list:
-        if df_has_nans(df, [name]):
+    """ Internal function that raises an exception if user passed in a dataframe with NANs in a particular column (where col names are in col_list)
+    """
+    new_col_list = _scalar_or_list_to_list(col_list)
+    for name in new_col_list:
+        if df_columns_has_nans(df, [name]):
             raise TypeError('Found unexpected NaN values in column "{0}" of DataFrame "{1}".'.format(name, df_name))
 
 def df_columns_has_nans(df, col_list):
-    for name in col_list:
+    """ Internal function that returns true if a column in col_list has NANs
+    """
+    new_col_list = _scalar_or_list_to_list(col_list)
+    for name in new_col_list:
+        if name not in df.keys():
+            raise TypeError('Test for NAN in column %s that does not exist in dataframe' % (name))
         if df[name].isnull().values.any():
             return True
+    
     return False
 

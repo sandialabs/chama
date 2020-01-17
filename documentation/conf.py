169aaedddbd7685aa29fb01b02fa06c983bf3834
kaklise <kaklise@sandia.gov>
2020-01-17 13:15:14 -0700

    Minor updates

--- documentation/conf.py
+++ documentation/conf.py
@@ -67,7 +67,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Chama'
-copyright = u'2016-2019, National Technology & Engineering Solutions of Sandia, LLC (NTESS)'
+copyright = u'2016 National Technology & Engineering Solutions of Sandia, LLC (NTESS)'
 author = u'Sandia National Laboratories'
 
 # The version info for the project you're documenting, acts as replacement for

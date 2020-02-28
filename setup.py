9dc58dced222ac47239e1f7964deacef91ae6b63
kaklise <kaklise@sandia.gov>
2020-02-28 14:46:59 -0700

    Added dependencies to setup.py

--- setup.py
+++ setup.py
@@ -11,6 +11,7 @@ AUTHOR = 'Chama developers'
 MAINTAINER_EMAIL = 'kaklise@sandia.gov'
 LICENSE = 'Revised BSD'
 URL = 'https://github.com/sandialabs/chama'
+DEPENDENCIES = ['pyomo', 'pandas', 'numpy', 'scipy']
 
 # use README file as the long description
 file_dir = os.path.abspath(os.path.dirname(__file__))
@@ -32,13 +33,6 @@ with open(os.path.join(file_dir, 'chama', '__init__.py')) as f:
     else:
         raise RuntimeError("Unable to find version string.")
         
-setuptools_kwargs = {
-    'zip_safe': False,
-    'install_requires': [],
-    'scripts': [],
-    'include_package_data': True
-}
-
 setup(name=DISTNAME,
       version=VERSION,
       packages=PACKAGES,
@@ -49,4 +43,7 @@ setup(name=DISTNAME,
       maintainer_email=MAINTAINER_EMAIL,
       license=LICENSE,
       url=URL,
-      **setuptools_kwargs)
+      zip_safe=False,
+      install_requires=DEPENDENCIES,
+      scripts=[],
+      include_package_data=True)
